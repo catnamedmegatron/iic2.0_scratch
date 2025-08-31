c256930ae84963dc5d90309a7ec145ee175ba9ee
Shlok Mathurkar <shlokmathurkar@gmail.com>
2025-08-31 17:00:03 +0530

    home.css file creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './My_components'
 
-function App() {
+export const App = () => {
   return (
-    
-  );
+    <div className='container'>
+      <Home/>
+    </div>
+  )
 }
-
-export default App;
---
+++ src/My_components/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Home.css'
+
+export const home = () => {
+  return (
+    <div>home</div>
+  )
+}
